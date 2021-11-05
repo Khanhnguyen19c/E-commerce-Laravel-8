a02fdb13125d7553fa5ef89ae95072f37edc07c4
Khanh Nguyen <khanhnguyen19c@gmail.com>
2021-11-05 12:18:03 +0700

    update lang

---
+++ resources/lang/vn/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Bạn đã nhập sai tài khoản hoặc mật khẩu vui lòng kiểm tra lại',
+    'password' => 'Mật khẩu bạn nhập không chính xác',
+    'throttle' => 'Quá nhiều lần đăng nhập. Vui lòng thử lại sau: giây giây.',
+
+];
